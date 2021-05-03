abdcceff233bcffdc3a3d9db286ee8cae9532f0d
Matheus Tagomori <mathschmidtt@gmail.com>
2021-05-03 20:53:31 -0300

    created card component for the home page

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HomeCard from '../Component/HomeCard'
+
+export default function Home () {
+    return (
+        <div className="main">
+            <HomeCard/>
+        </div>
+    )
+}

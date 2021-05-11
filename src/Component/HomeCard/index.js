9eb8ddc18eb9cfe2a5866c918d3bff28a30cdc4a
Matheus Tagomori <mathschmidtt@gmail.com>
2021-05-11 17:09:54 -0300

    styled divs introduced

--- src/Component/HomeCard/index.js
+++ src/Component/HomeCard/index.js
@@ -3,21 +3,30 @@ import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import cardImg from '../../assets/rio-de-janeiro.png'
+import styled from "styled-components";
+
+const CardWrapper = styled.div`
+    width: 18rem;
+    margin-right: 15px;
+    box-shadow: 0.2px 0.2px rgba(173, 173, 173, 0.445);
+`
 
 class HomeCard extends React.Component {
     render() {
         return(
-            <Card style={{ width: '18rem', marginRight: '15px', boxShadow: '0.2px 0.2px rgba(173, 173, 173, 0.445)' }}>
-                <Card.Img variant="top" src={cardImg} />
-                <Card.Body>
-                    <h6>HOSPEDAGENS</h6>
-                    <h3>Rio de Janeiro</h3>
-                    <p>Café da manhã</p>
-                    <Button variant="primary">
-                        R$ 469
-                    </Button>
-                </Card.Body>
-            </Card>
+            <CardWrapper>
+                <Card>
+                    <Card.Img variant="top" src={cardImg} />
+                    <Card.Body>
+                        <h6>HOSPEDAGENS</h6>
+                        <h3>Rio de Janeiro</h3>
+                        <p>Café da manhã</p>
+                        <Button variant="primary">
+                            R$ 469
+                        </Button>
+                    </Card.Body>
+                </Card>
+            </CardWrapper>    
         )
     }
 }

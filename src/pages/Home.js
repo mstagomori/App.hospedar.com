f2a4ff6109d3b6d152062ab5d669af0da67514a0
Gabriel Ponte <gabrielponte@poli.ufrj.br>
2021-05-12 10:02:04 -0300

    added filter

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,61 @@ export default function Home () {
         <div className="main">
             <HomeNavBar/>
             <div className="filter-container">
+            <h4>Passagens aéreas com reserva flexível<br></br> </h4>
+                
+                {/* Data Ida: {HomeDate()}<br></br> Data Volta  {HomeDate()} */}
+            
+                <form>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                    <label for="inputEmail4">Origem</label>
+                    <input type="text" class="form-control" id="inputEmail4"/>
+                    </div>
+                    <div class="form-group col-md-6">
+                    <label for="inputPassword4">Destino</label>
+                    <input type="text" class="form-control" id="inputPassword4"/>
+                    </div>
+                    <div class="form-group col-md-6">
+                    <label for="inputEmail4">Data Ida</label>
+                    <input type="date" class="form-control" id="inputEmail4"/>
+                    </div>
 
+                    <div class="form-group col-md-6">
+                    <label for="inputEmail4">Data Volta</label>
+                    <input type="date" class="form-control" id="inputEmail4"/>
+                    </div>
+
+                    <div class="form-group col-md-6">
+                    <label for="inputEmail4">Número de passageiros</label>
+                    <select id="inputState" class="form-control">
+                        <option selected>Selecione</option>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                        <option>6</option>
+                        <option>7</option>
+                        <option>8</option>
+                    </select>
+                    </div>
+
+                    <div class="form-group col-md-6">
+                    <label for="inputPassword4">Classe</label>
+                    <select id="inputState" class="form-control">
+                        <option selected>Selecione</option>
+                        <option>Primeira Classe</option>
+                        <option>Executiva</option>
+                        <option>Econômica</option>
+                    </select>
+                    </div>
+                    
+                </div>
+                <div class="w-100 d-flex justify-content-end">
+                    <button type="submit" class="btn btn-primary">Submeter</button>
+                </div>
+                
+                </form>
             </div>
 
             <div className="offer-container">

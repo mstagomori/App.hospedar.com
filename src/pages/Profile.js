375c7383f73e6a5d2e2709e385560b13fe6f47a5
Gabriel Ponte <gabrielponte@poli.ufrj.br>
2021-05-14 15:33:48 -0300

    realização de pagamentos com alterações no navbarprofile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,172 +1,102 @@
-import React, { useState } from "react";
-import NavBarProfile from "../Component/NavBarProfile";
-import styled from "styled-components";
+import React, { useState } from 'react'
+import NavBarProfile from '../Component/NavBarProfile'
+import styled from 'styled-components'
 
 const FilterWrapper = styled.div`
-  width: 100%;
-  height: fit-content;
-  display: flex;
-  flex-direction: column;
-  background-color: #222c69;
-  color: white;
-  border-radius: 10px;
-  padding: 20px;
-  font-family: "Roboto", sans-serif;
-  margin: 10px 10px 10px 10px;
+    width: 100%;
+    height: fit-content;
+    display: flex;
+    flex-direction: column;
+    background-color: #222c69;
+    color: white;
+    border-radius: 10px;
+    padding: 20px;
+    font-family: 'Roboto', sans-serif;
+    margin: 10px 10px 10px 10px;
 
-  @media (min-width: 1200px) {
-    max-width: 1200px;
-
-  }
-`;
+    @media(min-width: 1200px) {
+        max-width: 1200px;
+    }
+`
 
 export default function Profile() {
-  return (
-    <div className="main">
-      <NavBarProfile type="Tipo"/>
+    return (
+        <div className="main">
+            <NavBarProfile type="Tipo"/>
+                <div className="filter-container">
+                <FilterWrapper>
+                    <h2>Minha Conta</h2>
+                        <form>
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                <label for="inputEmail4">Nome</label>
+                                <input type="text" readonly class="form-control" bg = "white" id="staticEmail" value="Cláudio"></input>
+                                
+                                </div>
+                                <div class="form-group col-md-6">
+                                <label for="inputPassword4">Sobrenome</label>
+                                <input type="text" readonly class="form-control" bg = "white" id="staticEmail" value="Miceli"></input>
+                                </div>
+                                <div class="form-group col-md-6">
+                                <label for="inputEmail4">Email</label>
+                                <input type="text" readonly class="form-control" bg = "white" id="staticEmail" value="claudiomiceli@poli.ufrj.br"></input>
+                                </div>
 
-      <div className="filter-container">
-        <FilterWrapper>
-          <h2>Minha Conta</h2>
-          <form>
-            <div class="form-row">
-              <div class="form-group col-md-6">
-                <label for="inputEmail4">Nome</label>
-                <input
-                  type="text"
-                  readonly
-                  class="form-control"
-                  bg="white"
-                  id="staticEmail"
-                  value="Cláudio"
-                ></input>
-              </div>
-              <div class="form-group col-md-6">
-                <label for="inputPassword4">Sobrenome</label>
-                <input
-                  type="text"
-                  readonly
-                  class="form-control"
-                  bg="white"
-                  id="staticEmail"
-                  value="Miceli"
-                ></input>
-              </div>
-              <div class="form-group col-md-6">
-                <label for="inputEmail4">Email</label>
-                <input
-                  type="text"
-                  readonly
-                  class="form-control"
-                  bg="white"
-                  id="staticEmail"
-                  value="claudiomiceli@poli.ufrj.br"
-                ></input>
-              </div>
+                                <div class="form-group col-md-6">
+                                <label for="inputEmail4">Telefone</label>
+                                <input type="text" readonly class="form-control" bg = "white" id="staticEmail" value="994421492"></input>
+                                {/* <input type="date" class="form-control" id="inputEmail4"/> */}
+                                </div>
+                                
+                                <div class="form-group col-md-6">
+                                <label for="inputEmail4">Data de Nascimento</label>
+                                <input type="text" readonly class="form-control" bg = "white" id="staticEmail" value="20/10/1990"></input>
+                                </div>
 
-              <div class="form-group col-md-6">
-                <label for="inputEmail4">Telefone</label>
-                <input
-                  type="text"
-                  readonly
-                  class="form-control"
-                  bg="white"
-                  id="staticEmail"
-                  value="994421492"
-                ></input>
-                {/* <input type="date" class="form-control" id="inputEmail4"/> */}
-              </div>
+                                <div class="form-group col-md-6">
+                                <label for="inputPassword4">Ultimos 4 digitos do Cartão de Crédito</label>
+                                <input type="text" readonly class="form-control" bg = "white" id="staticEmail" value="2312"></input>
+                                </div>
+                                
+                            </div>
+                            <div class="w-100 d-flex justify-content-end">
+                                <a href="/changeprofile" type="submit" class="btn btn-danger">Editar</a>
+                            </div>
+                        
+                        </form>
+                    <h2>Minhas Viagens</h2>     
+                        <form>
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                <label for="inputEmail4">Voo</label>
+                                <input type="text" readonly class="form-control" bg = "white" id="staticEmail" value="XYZ"></input>
+                                
+                                </div>
+                                <div class="form-group col-md-6">
+                                <label for="inputPassword4">Horário</label>
+                                <input type="text" readonly class="form-control" bg = "white" id="staticEmail" value="HH:MM"></input>
+                                </div>
+                                <div class="form-group col-md-6">
+                                <label for="inputEmail4">Assentos</label>
+                                <input type="text" readonly class="form-control" bg = "white" id="staticEmail" value="B3,B4,B5"></input>
+                                </div>
 
-              <div class="form-group col-md-6">
-                <label for="inputEmail4">Data de Nascimento</label>
-                <input
-                  type="text"
-                  readonly
-                  class="form-control"
-                  bg="white"
-                  id="staticEmail"
-                  value="20/10/1990"
-                ></input>
-              </div>
+                                <div class="form-group col-md-6">
+                                <label for="inputEmail4">Valor</label>
+                                <input type="text" readonly class="form-control" bg = "white" id="staticEmail" value="R$ 500,00"></input>
+                                {/* <input type="date" class="form-control" id="inputEmail4"/> */}
+                                </div>
+                                
+                            </div>
+                            <div class="w-100 d-flex justify-content-end">
+                                <a href="/airplane" type="submit" class="btn btn-danger">Alterar minha viagem</a>
+                            </div>
+                        
+                        </form>
 
-              <div class="form-group col-md-6">
-                <label for="inputPassword4">
-                  Ultimos 4 digitos do Cartão de Crédito
-                </label>
-                <input
-                  type="text"
-                  readonly
-                  class="form-control"
-                  bg="white"
-                  id="staticEmail"
-                  value="2312"
-                ></input>
-              </div>
+                </FilterWrapper>
             </div>
-            <div class="w-100 d-flex justify-content-end">
-              <a href="/changeprofile" type="submit" class="btn btn-danger">
-                Editar
-              </a>
-            </div>
-          </form>
-          <h2>Minhas Viagens</h2>
-          <form>
-            <div class="form-row">
-              <div class="form-group col-md-6">
-                <label for="inputEmail4">Voo</label>
-                <input
-                  type="text"
-                  readonly
-                  class="form-control"
-                  bg="white"
-                  id="staticEmail"
-                  value="XYZ"
-                ></input>
-              </div>
-              <div class="form-group col-md-6">
-                <label for="inputPassword4">Horário</label>
-                <input
-                  type="text"
-                  readonly
-                  class="form-control"
-                  bg="white"
-                  id="staticEmail"
-                  value="HH:MM"
-                ></input>
-              </div>
-              <div class="form-group col-md-6">
-                <label for="inputEmail4">Assentos</label>
-                <input
-                  type="text"
-                  readonly
-                  class="form-control"
-                  bg="white"
-                  id="staticEmail"
-                  value="B3,B4,B5"
-                ></input>
-              </div>
-
-              <div class="form-group col-md-6">
-                <label for="inputEmail4">Valor</label>
-                <input
-                  type="text"
-                  readonly
-                  class="form-control"
-                  bg="white"
-                  id="staticEmail"
-                  value="R$ 500,00"
-                ></input>
-                {/* <input type="date" class="form-control" id="inputEmail4"/> */}
-              </div>
-            </div>
-            <div class="w-100 d-flex justify-content-end">
-              <a href="/airplane" type="submit" class="btn btn-danger">
-                Alterar minha viagem
-              </a>
-            </div>
-          </form>
-        </FilterWrapper>
-      </div>
-    </div>
-  );
+           
+        </div>
+    )
 }

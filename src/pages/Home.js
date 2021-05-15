954769064e91794e23a77463701550203909acf5
Gabriel Ponte <gabrielponte@poli.ufrj.br>
2021-05-15 11:53:49 -0300

    Alteracoes na home, navbarprofile e lista com dados selecionadoss

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,28 @@
 import React, { useState } from 'react'
+import {importedStates} from './../Data/states.js';
 import Carousel from 'react-elastic-carousel'
-import NavBarProfile from '../Component/NavBarProfile'
+import NavBar from '../Component/NavBar'
 import HomeCard from '../Component/HomeCard'
 import styled from 'styled-components'
-import classe from '../Component/StatesList'
-
 
 const FilterWrapper = styled.div`
-    width: 100%;
-    height: fit-content;
-    display: flex;
-    flex-direction: column;
-    background-color: #222c69;
-    color: white;
-    border-radius: 10px;
-    padding: 20px;
-    font-family: 'Roboto', sans-serif;
-    margin: 10px 10px 10px 10px;
+		width: 100%;
+		height: fit-content;
+		display: flex;
+		flex-direction: column;
+		background-color: #222c69;
+		color: white;
+		border-radius: 10px;
+		padding: 20px;
+		font-family: 'Roboto', sans-serif;
+		margin: 10px 10px 10px 10px;
 
-    @media(min-width: 1200px) {
-        max-width: 1200px;
-    }
+		@media(min-width: 1200px) {
+				max-width: 1200px;
+		}
 `
 
-
-
-export default function Home(){
+export default function Home () {
 
 	const [states, setStates] = useState([]);
 	const [items, setItems] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
@@ -36,106 +33,93 @@ export default function Home(){
 		{ width: 768, itemsToShow: 3 },
 		{ width: 1200, itemsToShow: 4 }
 	];
-
-    function guessState(parcialString){
+	function guessState(parcialString){
 		let statesWithParcialString = [];
 		importedStates.forEach(x=> { if (x["Nome"].toLowerCase().includes(parcialString))  statesWithParcialString.push(x) } )
 		setStates(statesWithParcialString)
 	}
 
-    return (
-        <div className="main">
-            <NavBarProfile/>
-            
-                <div className="filter-container">
-                <FilterWrapper>
-                <h4>Passagens aéreas com reserva flexível</h4>
-                        <form onSubmit={classe.handleSubmit}>
-                            <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="inputPassword4">Origem</label>
-                                <select class="form-control" value={classe.state.value} onChange={classe.handleChangeOrigin}> 
-                                <option name="test" value="selecione">Selecione</option>
-                                <option name="test" value="rio de janeiro">Rio de Janeiro</option>
-                                <option name="test" value="são paulo">São Paulo</option>
-                                <option name="test" value="recife">Recife</option>
-                                <option name="test" value="fortaleza">Fortaleza</option>
-                                </select>
-                                </div>
+	return (
+		<div className="main">
+		<NavBar/>
 
-                                <div class="form-group col-md-6">
-                                <label for="inputPassword4">Destino</label>
-                                <select class="form-control" value={classe.state.value} onChange={classe.handleChangeDestination}> 
-                                <option name="test" value="selecione">Selecione</option>
-                                <option name="test" value="rio de janeiro">Rio de Janeiro</option>
-                                <option name="test" value="são paulo">São Paulo</option>
-                                <option name="test" value="recife">Recife</option>
-                                <option name="test" value="fortaleza">Fortaleza</option>
-                                </select>
-                                </div>
-
-                                <div class="form-group col-md-4">
-                                <label for="inputPassword4">Data Ida</label>
-                                <input type="date" class="form-control" id="inputPassword4" value={classe.state.value} onChange={classe.handleChangeDataInit} />
-                                </div>
-
-                                <div class="form-group col-md-4">
-                                <label for="inputPassword4">Data Volta</label>
-                                <input type="date" class="form-control" id="inputPassword4" value={classe.state.value} onChange={classe.handleChangeDataEnd}/>
-                                </div>
-
-                                <div class="form-group col-md-4">
-                                <label for="inputPassword4">Número de passageiros</label>
-                                <select class="form-control" value={classe.state.value} onChange={classe.handleChangeNumberPassengers}> 
-                                    <option name="test" value="selecione">Selecione</option>
-                                    <option name="test" value="1">1</option>
-                                    <option name="test" value="2">2</option>
-                                    <option name="test" value="3">3</option>
-                                    <option name="test" value="4">4</option>
-                                    <option name="test" value="5">5</option>
-                                    <option name="test" value="6">6</option>
-                                    <option name="test" value="7">7</option>
-                                    <option name="test" value="8">8</option>
-                                </select>
-                                </div>
-
-                                {/* <div class="form-group col-md-6">
-                                <label for="inputPassword4">Classe</label>
-                                <select id="inputPassword4" class="form-control">
-                                    <option selected>Selecione</option>
-                                    <option>Primeira Classe</option>
-                                    <option>Executiva</option>
-                                    <option>Econômica</option>
-                                </select>
-                                </div> */}
-                                
-                            </div>
-                            <div class="w-100 d-flex justify-content-end">
-                                <a href="/destinations" type="submit" class="btn btn-danger">Buscar</a>
-                            </div>
-                        
-                        </form>
-                </FilterWrapper>
-            </div>
-
-            <div className="offer-container">
-                <div className="offer-description-container">
-                    <h6>Aproveite</h6>
-                    <h3>Rio de Janeiro, a cidade maravilhosa</h3>
-                </div>
-                
-                
-                <div className="carousel-container">
-                    <Carousel breakPoints={breakPoints}>
-                    {items.map((item) => (
-                        <HomeCard key={item}>{item}</HomeCard>
-                    ))}
-                    </Carousel>
-                </div>
-            </div>
-            
+		<div className="filter-container">
+		<FilterWrapper>
+		<h4>Passagens aéreas com reserva flexível</h4>
+		<form>
+		<div class="form-row">
+		<div class="form-group col-md-6">
+		<label for="inputPassword3" >Origem</label>
+		<input type="text" class="form-control" id="inputPassword4" onChange={event => guessState(event.target.value.toLowerCase())} list="cities"/>
+		<datalist id="cities">
+            {states.map( (item, key) =>
+            <option key={key} value={item.Nome} /> )}
+        </datalist>
         </div>
-        
-    ) 
-}
+		<div class="form-group col-md-6">
+		<label for="inputPassword4">Destino</label>
+		<input type="text" class="form-control" id="inputPassword4" onChange={event => guessState(event.target.value.toLowerCase())} list="cities"/>
+		</div>
+		<div class="form-group col-md-6">
+		<label for="inputPassword4">Data Ida</label>
+		<input type="date" class="form-control" id="inputPassword4"/>
+		</div>
 
+		<div class="form-group col-md-6">
+		<label for="inputPassword4">Data Volta</label>
+		<input type="date" class="form-control" id="inputPassword4"/>
+		</div>
+
+		<div class="form-group col-md-6">
+		<label for="inputPassword4">Número de passageiros</label>
+		<select id="inputPassword4" class="form-control">
+		<option selected>Selecione</option>
+		<option>1</option>
+		<option>2</option>
+		<option>3</option>
+		<option>4</option>
+		<option>5</option>
+		<option>6</option>
+		<option>7</option>
+		<option>8</option>
+		</select>
+		</div>
+
+		<div class="form-group col-md-6">
+		<label for="inputPassword4">Classe</label>
+		<select id="inputPassword4" class="form-control">
+		<option selected>Selecione</option>
+		<option>Primeira Classe</option>
+		<option>Executiva</option>
+		<option>Econômica</option>
+		</select>
+		</div>
+
+		</div>
+		<div class="w-100 d-flex justify-content-end">
+		<a href="/destinations" type="submit" class="btn btn-danger">Buscar</a>
+		</div>
+
+		</form>
+		</FilterWrapper>
+		</div>
+
+		<div className="offer-container">
+		<div className="offer-description-container">
+		<h6>Aproveite</h6>
+		<h3>Rio de Janeiro, a cidade maravilhosa</h3>
+		</div>
+
+
+		<div className="carousel-container">
+		<Carousel breakPoints={breakPoints}>
+		{items.map((item) => (
+			<HomeCard key={item}>{item}</HomeCard>
+		))}
+		</Carousel>
+		</div>
+		</div>
+
+		</div>
+	)
+}
